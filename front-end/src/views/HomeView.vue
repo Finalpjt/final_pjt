e7b09f4f5515ec0ab1ce9e505ac12b82c29f6393
hyun940415 <hyun940415@naver.com>
2023-05-17 17:34:03 +0900

    05_17

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    
+    <ul class="">
+      <!-- 비 로그인 시  -->
+      <div class="navbar-nav ml-auto" v-if="!currentUser">
+        <li class="nav-item">
+          <router-link to="/signup">
+            <font-awesome-icon icon="user-plus" /> Sign Up
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <form @submit.prevent="logIn">
+            <label for="username">username : </label>
+            <input type="text" id="username" v-model="username"><br>
+
+            <label for="password"> password : </label>
+            <input type="password" id="password" v-model="password"><br>
+
+            <input type="submit" value="logIn">
+          </form>
+        </li>
+      </div>
+      <!-- 로그인 시 -->
+      <div class="navbar-nav ml-auto" v-if="currentUser">
+        <li class="nav-item">
+          <router-link to="/profile">
+            <font-awesome-icon icon="user" />
+            {{currentUser.username}}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a href class="nav-link" @click="logout">
+            <font-awesome-icon icon="sign-out-alt" /> LogOut
+          </a>
+        </li>
+      </div>
+    </ul>
+    <MovieList/>
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    MovieList,
+  },
+  data() {
+    return {
+      username: null,
+      password: null,
+    }
+  },
+  computed: {
+    currentUser () {
+      return this.$store.state.auth.user
+    }
+  },
+  methods: {
+    logOut () {
+      this.$store.dispatch('auth/logout')
+      this.$router.push('/')
+    },
+    logIn() {
+      const username = this.username
+      const password = this.password
+
+      const payload = {
+        username, password
+      }
+
+      this.$store.dispatch('login', payload)
+
+    }
   }
 }
 </script>
